ff2ebb80bdca975508f4be54bb0917cabaf16a04
C H <ch@mac.lan>
2025-03-05 13:57:13 -0800

    Add deploy script

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import { ChevronDown, Leaf, Pill, Droplet, LineChart } from "lucide-react";
 import { getWasteItems, getAvailableDrugs, type WasteItem, type DrugOption } from "./utils/wasteCalculator";
 
